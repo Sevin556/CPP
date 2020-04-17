4e156d5e81a0ec98e543d2b5639a7937a03f8459
Sevin556 <56001554+Sevin556@users.noreply.github.com>
2020-04-18 01:49:11 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,11 +3,14 @@
 #include <QObject>
 #include <QDebug>
 #include <QFile>
+#include <QGraphicsScene>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    qDebug() << "Idem na to";
     createMap();
 }
 
@@ -18,6 +21,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::createMap()
 {
+    auto * scene = new QGraphicsScene(ui->graphicsView);
+    ui->graphicsView->setScene(scene);
     QFile file("test.txt");
 
     if(!file.open(QIODevice::ReadOnly))
@@ -28,12 +33,22 @@ void MainWindow::createMap()
 
     QTextStream instream(&file);
     QString line = instream.readLine(50);
-    QStringList splitedLine= line.split(" ");
-    int x1 = splitedLine[0].toInt();
-    int y1 = splitedLine[1].toInt();
-    int x2 = splitedLine[2].toInt();
-    int y2 = splitedLine[3].toInt();
-    qDebug() << "first line: " << x1 << y1 << x2 << y2;
+    while (line != nullptr){
+        if (line[0] == "#"){
+            line = instream.readLine(50);
+        }
+        QStringList splitedLine= line.split(" ");
+
+        int x1 = splitedLine[0].toInt();
+        int y1 = splitedLine[1].toInt();
+        int x2 = splitedLine[2].toInt();
+        int y2 = splitedLine[3].toInt();
+        qDebug() << "first line: " << x1 << y1 << x2 << y2;
+        scene->addLine(x1,y1,x2,y2,QPen({Qt::black},4));
+        line = instream.readLine(50);
+    }
+
+
     file.close();
     return;
 
